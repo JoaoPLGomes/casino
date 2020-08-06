8d353af6b49c0d7c42fc0057485c0ac7294995cc
joao.gomes <joao.gomes@runtime-revolution.com>
2020-08-06 23:03:28 +0100

    Create function for first exercise

--- casino-backend/index.js
+++ casino-backend/index.js
@@ -1,8 +1,22 @@
-const express = require('express')
+const express = require('express');
+var request = require('request');
 const app = express();
+
+const BASE_URL = `https://restcountries.eu/rest/v2/name/{name}`;
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+});
+
+app.get('/testRest', (req, res) => {
+  request({
+    method: 'GET',
+    uri: BASE_URL.replace('{name}',req.query.name),
+  }, function (error, response, body){
+    if(!error && response.statusCode == 200){
+      res.json(body);
+    }
+  })
 });
 
 app.listen(8000, () => {
