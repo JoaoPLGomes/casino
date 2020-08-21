9e892a47fe8441513a100a7eb31e378b2f9fe3bd
joao.gomes <joao.gomes@runtime-revolution.com>
2020-08-21 13:51:20 +0100

    Fix error handling and added validator package

--- casino-backend/App/controllers/Countries.ts
+++ casino-backend/App/controllers/Countries.ts
@@ -2,14 +2,32 @@
 /* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 import express from 'express';
 import axios from 'axios';
+import validator from 'validator';
 import { BASE_URL } from '../contants';
 
-const fetchURL = (url: string) => axios.get(url).catch(() => 'No results');
+const fetchURL = (url: string) => axios.get(url).catch((err) => console.log(err));
 
 export const list_countries = (req: express.Request, res: express.Response) => {
-    axios.get(BASE_URL.replace('{name}', <string>req.query.name ?? '')).then((response) => {
+  if (validator.isAlpha(<string>req.query.name)) {
+    axios.get(BASE_URL.replace('{name}', <string>req.query.name ?? ''))
+    .then((response) => {
         res.json(response.data);
-    }).catch((err) => res.json(`Error: ${err.name}`));
+    }, () => res.json([]))
+    .catch((err) => {
+      if (err.response) {
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+      } else if (err.request) {
+        console.log(err.request);
+      } else {
+        console.log('Error', err.message);
+      }
+      console.log(err.config);
+    });
+  } else {
+    res.json([])
+  }
 };
 
 export const list_array_countries = (req: express.Request, res: express.Response) => {
@@ -20,5 +38,16 @@ export const list_array_countries = (req: express.Request, res: express.Response
         .then((data) => {
             res.json(data.map((obj: any) => (obj.data ? obj.data.map((arrObj: any) => arrObj.name) : null)));
         })
-        .catch((err) => res.json(`Error: ${err.name}`));
+        .catch((err) => {
+          if (err.response) {
+            console.log(err.response.data);
+            console.log(err.response.status);
+            console.log(err.response.headers);
+          } else if (err.request) {
+            console.log(err.request);
+          } else {
+            console.log('Error', err.message);
+          }
+          console.log(err.config);
+        });
 };
